836d593fa9ba7f4e1dc163e1505ab34e393870b6
Naresh Kumar <nareshkumar0000002@gmail.com>
2024-11-23 13:42:08 +0530

    code

--- tailwindproms/src/App.jsx
+++ tailwindproms/src/App.jsx
@@ -2,33 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/card'
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21,
+  }
+  let newArr = [1,2,3]
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
-  <div class="pt-6 md:p-8 text-center space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+<Card username = "Chai aur Code" />
+  <Card username="hitesh"/>
+
     </>
   )
 }
